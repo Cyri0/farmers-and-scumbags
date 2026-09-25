474320f572c130880d3a99a486fada95c6d00473
1/3 NT <you@example.com>
2026-09-25 11:08:51 +0200

    Refactor map generation logic in useMapStore and update CSS for game area

--- src/store/useMapStore.ts
+++ src/store/useMapStore.ts
@@ -6,46 +6,117 @@ type MapStoreType = {
 }
 
 export const useMapStore = create<MapStoreType>((set)=>({
-    map: generateMap(10)
+    map: generateMap(50,50)
 }))
 
-function addGrass(map: TileType[][]):TileType[][]{
-    let baseGrass = []
-    for(let r = 0; r < map.length; r++){
-        for(let c = 0; c < map[r].length; c++){
-            if(Math.random() < 0.2){
-                map[r][c].ground = "grass"
-                baseGrass.push([r,c])
+
+function addGrass(map: TileType[][]): void {
+    const height = map.length;
+    const width = map[0].length;
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (Math.random() < 0.1) {
+                map[i][j].ground = "grass";
             }
         }
     }
 
-    baseGrass.forEach(cord => {
-        const x = cord[0]
-        const y = cord[1]
-
-        if(x > 0 && Math.random() < 0.5){ map[x-1][y].ground = "grass" }
-        if(x < map.length-1 && Math.random() < 0.5){ map[x+1][y].ground = "grass" }
-
-        if(y > 0 && Math.random() < 0.5){ map[x][y-1].ground = "grass" }
-        if(y < map.length-1 && Math.random() < 0.5){ map[x][y+1].ground = "grass" }
-    })
-
-    return map;
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (map[i][j].ground === "grass") {
+                if (Math.random() < 0.6 && i > 0 && i < height - 1 && j > 0 && j < width - 1) {
+                    map[i-1][j].ground = "grass";
+                    map[i+1][j].ground = "grass";
+                    map[i][j-1].ground = "grass";
+                    map[i][j+1].ground = "grass";
+                }
+            }
+        }
+    }
 }
 
-function generateMap(size: number):TileType[][]{
-    let tempMap:TileType[][] = []
-    for(let r = 0; r < size; r++){
-        let line:TileType[] = []
-        for(let c = 0; c < size; c++){
-            line.push({
-                building: null,
-                ground: "water", 
-                resource: null
-            })
+function addSand(map: TileType[][]): void {
+    const height = map.length;
+    const width = map[0].length;
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if(map[i][j].ground === "water" && i < height - 1 && j < width - 1) {
+                if ((i > 0 && map[i-1][j].ground === "grass") || 
+                    (i < height - 1 && map[i+1][j].ground === "grass") || 
+                    (j > 0 && map[i][j-1].ground === "grass") || 
+                    (j < width - 1 && map[i][j+1].ground === "grass")) {
+                    map[i][j].ground = "sand";
+                }
+            }
         }
-        tempMap.push(line)
     }
-    return addGrass(tempMap)
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (map[i][j].ground === "sand" && i < height - 1 && j < width - 1) {
+                if ((i > 0 && map[i-1][j].ground !== "water") && 
+                    (i < height - 1 && map[i+1][j].ground !== "water") && 
+                    (j > 0 && map[i][j-1].ground !== "water") && 
+                    (j < width - 1 && map[i][j+1].ground !== "water")) {
+                    map[i][j].ground = "grass";
+                }
+            }
+        }
+    }
+
+}
+
+function addStone(map: TileType[][]): void {
+    const height = map.length;
+    const width = map[0].length;
+    
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (Math.random() < 0.01 && map[i][j].ground === "grass") {
+                map[i][j].ground = "stone";
+            }
+        }
+    }
+
+    for(let iteration = 0; iteration < 3; iteration++){
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                if(Math.random() < 0.5){
+                    if(map[i][j].ground === "stone" && i > 1 && map[i-1][j].ground == "grass"){ map[i-1][j].ground = "stone" }
+                }
+                if(Math.random() < 0.5){
+                    if(map[i][j].ground === "stone" && i > 1 && j > 1 && map[i-1][j-1].ground == "grass"){ map[i-1][j-1].ground = "stone" }
+                }
+                if(Math.random() < 0.5){
+                    if(map[i][j].ground === "stone" && i > 1 && j > 1 && map[i][j-1].ground == "grass"){ map[i][j-1].ground = "stone" }
+                }
+
+                if(Math.random() < 0.5){
+                    if(map[i][j].ground === "stone" && i > 1 && j > 1 && map[i-1][j-1].ground == "grass"){ map[i-1][j-1].ground = "stone" }
+                }
+
+
+            }
+        }
+    }
+}
+
+export function generateMap(width: number, height: number): TileType[][] {
+    const map: TileType[][] = [];
+    for (let i = 0; i < height; i++) {
+        const row: TileType[] = [];
+        for (let j = 0; j < width; j++) {
+            row.push({
+                ground: "water",
+                resource: null,
+                building: null
+            });
+        }
+        map.push(row);
+    }
+
+    addGrass(map);
+    addSand(map);
+    addStone(map);
+    return map;
 }
